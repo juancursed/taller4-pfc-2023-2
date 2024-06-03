fbe4031a5eea211052ec48e24da9a1f31cb536a1
JuanCamiloGutiV <144349089+JuanCamiloGutiV@users.noreply.github.com>
2024-06-03 13:04:50 -0500

    Add files via upload

--- app/src/main/scala/taller4/Benchmarking.scala
+++ app/src/main/scala/taller4/Benchmarking.scala
@@ -1,5 +1,8 @@
 package taller4
+
 import org.scalameter._
+import taller4.Newton
+import taller4.NewtonParalelo
 
 object Benchmarking {
   def benchmark[T](name: String)(block: => T): Double = {
@@ -14,4 +17,36 @@ object Benchmarking {
     println(s"$name: $time ms")
     time
   }
+
+  def main(args: Array[String]): Unit = {
+    val expr1 = Newton.Suma(Newton.Atomo('x'), Newton.Numero(2))
+    val expr2 = Newton.Prod(Newton.Atomo('x'), Newton.Atomo('x'))
+    val expr3 = Newton.Suma(expr1, Newton.Expo(expr2, Newton.Numero(5)))
+    val expr4 = Newton.Logaritmo(Newton.Atomo('x'))
+    val expr5 = Newton.Prod(Newton.Div(expr1, expr2), Newton.Resta(expr3, expr4))
+    val expr6 = Newton.Expo(Newton.Atomo('x'), Newton.Numero(3))
+
+    val e1 = Newton.Resta(Newton.Prod(Newton.Atomo('x'), Newton.Atomo('x')), Newton.Numero(2.0))
+    val e2 = Newton.Resta(Newton.Prod(Newton.Atomo('x'), Newton.Atomo('x')), Newton.Numero(4.0))
+    val e3 = Newton.Suma(Newton.Resta(Newton.Prod(Newton.Atomo('x'), Newton.Atomo('x')), Newton.Numero(4.0)), Newton.Prod(Newton.Numero(3.0), Newton.Atomo('x')))
+
+    println("Benchmarking para limpiar")
+    benchmark("Secuencial limpiar") {
+      (1 to 20).foreach(_ => Newton.limpiar(expr5))
+    }
+
+    benchmark("Paralelo limpiar") {
+      (1 to 20).foreach(_ => NewtonParalelo.limpiar(expr5))
+    }
+
+    println("Benchmarking para derivar")
+    benchmark("Secuencial derivar") {
+      (1 to 20).foreach(_ => Newton.derivar(e1, Newton.Atomo('x')))
+    }
+
+    benchmark("Paralelo derivar") {
+      (1 to 20).foreach(_ => NewtonParalelo.derivar(e1, Newton.Atomo('x')))
+    }
+  }
 }
+
